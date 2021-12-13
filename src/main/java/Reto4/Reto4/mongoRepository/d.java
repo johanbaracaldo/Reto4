c83c3249c431cb1f90992af35a088668f1a14097
Johan Baracaldo <johan.baracaldo.mt@correo.usa.edu.co>
2021-12-13 17:15:51 -0500

    reto 4

---
+++ src/main/java/Reto4/Reto4/mongoRepository/d.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reto4.Reto4.mongoRepository;
+
+/**
+ *
+ * @author johan
+ */
+public class d {
+    
+}
